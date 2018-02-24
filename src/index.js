68f93945c95ebc2ddd173a0669699504bab22e60
Ekaterina Pavlova <pavlova.ea.91@gmail.com>
2018-02-24 04:38:34 +0300

    Implement a method makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-// PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+let arrCoinObj = [{name : 'H', value : 50}, {name : 'Q', value : 25},{name : 'D', value : 10},{name : 'N', value : 5},{name : 'P', value : 1},];
+
+module.exports = function makeExchange(currency)
+{
+    let currExchObj = {};
+
+    if (currency >= 10000)
+    {
+        currExchObj.error = "You are rich, my friend! We don't have so much coins for exchange";
+    }
+    else if (currency === 0)
+    {
+        return currExchObj;
+    }
+    else
+    {
+        let currentCurr = 0;
+
+        for (var i = 0; i < arrCoinObj.length; i ++)
+        {
+            let coinsCount = 0;
+
+            if ((currentCurr = Math.floor(currency / arrCoinObj[i].value)) >= 1)
+            {
+                for (var j = 0; j < currentCurr; j++)
+                {
+                    coinsCount += 1;
+                }
+                currency -= currentCurr * arrCoinObj[i].value;
+            }
+            if (coinsCount > 0)
+            {
+                currExchObj[arrCoinObj[i].name] = coinsCount;
+            }
+        }
+    }
+    return currExchObj;
 }
+
